bc5f725ac4c230922548b7f5a7a645c6413c082c
HexVT <nohamvuemba@hotmail.com>
2026-06-25 15:21:57 +0200

    Refactor imports and reformat App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,21 @@
-import { Toaster } from "@/components/ui/toaster"
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClientInstance } from '@/lib/query-client'
-import NavigationTracker from '@/lib/NavigationTracker'
-import { pagesConfig } from './pages.config'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import PageNotFound from './components/PageNotFound';
-import { AuthProvider, useAuth } from '@/lib/AuthContext';
-import { DarkModeProvider } from '@/lib/DarkModeContext';
-import UserNotRegisteredError from '@/components/UserNotRegisteredError';
-import ProtectedRoute from '@/components/ProtectedRoute';
-import Subscribe from './pages/Subscribe';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import ForgotPassword from './pages/ForgotPassword';
-import ResetPassword from './pages/ResetPassword';
+import { Toaster } from "@/components/ui/toaster";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClientInstance } from "@/lib/query-client";
 
-const { Pages, Layout, mainPage } = pagesConfig;
-const mainPageKey = mainPage ?? Object.keys(Pages)[0];
-const MainPage = mainPageKey ? Pages[mainPageKey] : <></>;
+import NavigationTracker from "@/lib/NavigationTracker";
 
-const LayoutWrapper = ({ children, currentPageName }) => Layout ?
-  <Layout currentPageName={currentPageName}>{children}</Layout>
-  : <>{children}</>;
+import { pagesConfig } from "./pages.config";
 
-const AuthenticatedApp = () => {
-  // Render the main app
-  return (
-    <Routes>
-      {/* Public auth routes */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/reset-password" element={<ResetPassword />} />
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
-      {/* Protected app routes */}
-      <Route element={<ProtectedRoute unauthenticatedElement={<Navigate to="/login" replace />} />}>
-        <Route path="/" element={
-          <LayoutWrapper currentPageName={mainPageKey}>
-            <MainPage />
-          </LayoutWrapper>
-        } />
-        {Object.entries(Pages).map(([path, Page]) => (
-          <Route
-            key={path}
-            path={`/${path}`}
-            element={
-              <LayoutWrapper currentPageName={path}>
-                <Page />
-              </LayoutWrapper>
-            }
-          />
-        ))}
-        <Route path="/Subscribe" element={<LayoutWrapper currentPageName="Subscribe"><Subscribe /></LayoutWrapper>} />
-      </Route>
+import PageNotFound from "@/components/PageNotFound";
 
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
-  );
-};
+import { AuthProvider } from "@/lib/AuthContext";
+import { DarkModeProvider } from "@/lib/DarkModeContext";
 
-function App() {
-
-  return (
-    <DarkModeProvider>
-      <AuthProvider>
-        <QueryClientProvider client={queryClientInstance}>
-          <Router>
-            <NavigationTracker />
-            <AuthenticatedApp />
-          </Router>
-          <Toaster />
-        </QueryClientProvider>
-      </AuthProvider>
-    </DarkModeProvider>
-  )
-}
-
-export default App
+import ProtectedRoute from "@/components/ProtectedRoute";
+import Subscribe from "@/pages/Subscribe";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
+import ForgotPassword from "@/pages/ForgotPassword";
+import ResetPassword from "@/pages/ResetPassword";
